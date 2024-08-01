fd41d45ec2430f47a1e19ba73142ae39edd7637b
RobinDanek <r.danek@web.de>
2024-08-01 18:13:24 +0200

    Added augmentation options (rotation, flipping) to the  Random Choice Sampling

--- modules/LoadAndTrans/LoadingTransforming.py
+++ modules/LoadAndTrans/LoadingTransforming.py
@@ -179,3 +179,128 @@ def RandomChoicePadding( data_dir, label_dir, output_dir, target_number, array_f
     print("Random Padding Completed")
 
     return 
+
+
+########### RANDOM AUGMENTATIONS ###########
+
+def rotateTree(tree, angle):
+    # Define rotation matrix along vertical axis, since trees should not be turned to the side
+    rotation_matrix = np.array([
+        [np.cos(angle), -np.sin(angle), 0],
+        [np.sin(angle), np.cos(angle), 0],
+        [0, 0, 1]
+    ])
+    rotated_points = np.dot(tree, rotation_matrix)
+    return rotated_points
+
+def flipTree(tree):
+    # Flips the tree horitontally
+    tree[:, 0] = -tree[:, 0]  # Flip the x-coordinate
+    return tree
+
+
+def RandomChoiceAugmPadding( data_dir, label_dir, output_dir, target_number, array_format, num_draws, num_rots, horizontal_flip ):
+    np.random.seed(42)
+    # Create output directory if not created allready
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=False)
+
+    # Load the labels and the according trees
+    label_df = pd.read_csv( label_dir )
+    fractal_dims = label_df['fractal_dim'].to_numpy()
+
+    # Load the trees in label order 
+    tree_list = [f'{tree}.txt' for tree in label_df['tree_id']]
+
+    # Go over tree list
+    for i, t in enumerate(tree_list):
+        # Draw num_draw times per tree
+        for j in range(num_draws):
+            output_path = os.path.join( output_dir, t.replace('.txt', f'_{j}.npy') )
+            tree_path = os.path.join(data_dir, t)
+            tree = np.loadtxt(tree_path)
+
+            # Ensure that all trees have 3 dimensions
+            if tree.shape[1] != 3:
+                if j==0:
+                    print(f"Point cloud data in {t} does not have 3 coordinates per point, instead it has {tree.shape[1]} coordinates. Omitting unnecessary coordinates...")
+                tree = tree[:,:3]
+
+            tree_shape = tree.shape[0]
+
+            # Check for too small tree
+            if tree_shape < target_number:
+                # Initialize new tree
+                new_tree = tree
+
+                # Check how many times the tree has to be recreated to match the target number
+                # and append the tree so many times
+                full_replications = target_number // tree_shape 
+                for _ in range( full_replications - 1 ): # -1 somce the tree has been appended once already
+                    new_tree = np.vstack([ new_tree, tree ])
+
+                # New draw the remaining points randomly without replacement
+                number_remaining_points = target_number - new_tree.shape[0]
+                drawn_point_indices = np.random.choice( tree.shape[0], size=number_remaining_points, replace=False )
+                drawn_points = tree[ drawn_point_indices ]
+
+                new_tree = np.vstack([ new_tree, drawn_points ])
+                # Now save the filled tree
+                if new_tree.shape[0] != target_number:
+                    print("Tree is too small")
+            
+            # Check for too large trees
+            elif tree_shape > target_number:
+                # draw samples from tree without replacement
+                new_tree_indices = np.random.choice( tree.shape[0], size=target_number, replace=False )
+                new_tree = tree[ new_tree_indices ]
+
+                if new_tree.shape[0] != target_number:
+                    print("Tree is too large")
+
+            # If the tree has the exact size just save it
+            else:
+                new_tree = tree
+
+            # Save tree before augmentations and changing up the output path
+            # save cloud and label as .npy file
+            new_npy = np.array( [(new_tree, fractal_dims[i])], dtype=array_format )
+            np.save( output_path, new_npy )
+
+            # Save the horizontal flip of the drawn tree
+            if horizontal_flip == True:
+                aug_tree = flipTree( new_tree )
+                output_path = os.path.join( output_dir, t.replace('.txt', f'_{j}_flip.npy') )
+                new_npy = np.array( [(aug_tree, fractal_dims[i])], dtype=array_format )
+                np.save( output_path, new_npy )
+
+            # Now turn the trees and optionally flip them
+            for k in range(num_rots):
+                # Choose random turning angle
+                angle = np.random.uniform( 0, 2*np.pi )
+
+                aug_tree = rotateTree( new_tree, angle=angle )
+
+                # Adapt output names in case of horizontal flip
+                if horizontal_flip == True:
+                    # First the rotation...
+                    output_path = os.path.join( output_dir, t.replace('.txt', f'_{j}_{k}_0.npy') )
+                    new_npy = np.array( [(aug_tree, fractal_dims[i])], dtype=array_format )
+                    np.save( output_path, new_npy )
+                    # ... then the flip
+                    output_path = os.path.join( output_dir, t.replace('.txt', f'_{j}_{k}_1.npy') )
+                    aug_tree = flipTree( aug_tree )
+                    new_npy = np.array( [(aug_tree, fractal_dims[i])], dtype=array_format )
+                    np.save( output_path, new_npy )
+                
+                # Now output without horizontal flip
+                else:
+                    output_path = os.path.join( output_dir, t.replace('.txt', f'_{j}_{k}.npy') )
+                    new_npy = np.array( [(aug_tree, fractal_dims[i])], dtype=array_format )
+                    np.save( output_path, new_npy )
+
+                    
+
+    print("Random Augmented Padding Completed")
+
+    return 
